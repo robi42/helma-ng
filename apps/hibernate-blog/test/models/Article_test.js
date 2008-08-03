df73d4b5b3213b7f9cf90dc2f35fa53616825f8d
Robert Thurnher <r.thurnher@gmail.com>
2008-08-03 03:11:53 +0200

    adds test case methods for update and delete

--- apps/hibernate-blog/test/models/Article_test.js
+++ apps/hibernate-blog/test/models/Article_test.js
@@ -49,3 +49,85 @@ testCase.testCreate = function () {
 
    return;
 };
+
+
+testCase.testUpdate = function () {
+   var article = articleModel.Article.find("where a.title = 'Test Title'") ?
+                 articleModel.Article.find("where a.title = 'Test Title'")[0] : null;
+
+   if (!article) {
+      var user = userModel.User.find("where u.name = 'testUser'") ?
+                 userModel.User.find("where u.name = 'testUser'")[0] : null;
+
+      if (!user) {
+         var userData = {
+            name: 'testUser',
+            password: 'pass'
+         };
+         userModel.doCreate(userData);
+
+         user = userModel.User.find("where u.name = 'testUser'")[0];
+      }
+
+      var articleData = {
+         creator: user,
+         title: 'Test Title',
+         text: 'Some text.'
+      };
+      articleModel.doCreate(articleData);
+
+      article = articleModel.Article.find("where a.title = 'Test Title'")[0];
+   }
+
+   var articleUpdateData = {
+      id: article.id,
+      title: 'Another Test Title',
+      text: 'Some other text.'
+   };
+   articleModel.doUpdate(articleUpdateData);
+
+   assertNotNull(article);
+   assertEqual(article.title, 'Another Test Title');
+   assertEqual(article.text, 'Some other text.');
+   assertEqual(article.creator.name, 'testUser');
+   assertEqual(article.getCreatorName(), 'testUser');
+
+   return;
+};
+
+testCase.testDelete = function () {
+   var article = articleModel.Article.find("where a.title = 'Test Title'") ?
+                 articleModel.Article.find("where a.title = 'Test Title'")[0] : null;
+
+   if (!article) {
+      var user = userModel.User.find("where u.name = 'testUser'") ?
+                 userModel.User.find("where u.name = 'testUser'")[0] : null;
+
+      if (!user) {
+         var userData = {
+            name: 'testUser',
+            password: 'pass'
+         };
+         userModel.doCreate(userData);
+
+         user = userModel.User.find("where u.name = 'testUser'")[0];
+      }
+
+      var articleData = {
+         creator: user,
+         title: 'Test Title',
+         text: 'Some text.'
+      };
+      articleModel.doCreate(articleData);
+
+      article = articleModel.Article.find("where a.title = 'Test Title'")[0];
+   }
+
+   articleModel.doDelete(article.id);
+
+   article = articleModel.Article.find("where a.title = 'Test Title'")[0];
+
+   assertUndefined(article);
+
+   return;
+};
