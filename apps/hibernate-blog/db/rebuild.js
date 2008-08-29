529b038477553940def0bd9ae297abc2d8c3d922
Robert Thurnher <r.thurnher@gmail.com>
2008-08-29 19:42:50 +0200

    rm old db schema rebuilding script

--- apps/hibernate-blog/db/rebuild.js
+++
@@ -1,9 +0,0 @@
-importJar('lib/mysql-connector-java-5.1.6-bin.jar');
-
-importModule('helma.hibernate', 'db');
-
-
-if (__name__ == '__main__') {
-   db.setConfigPath('config/dev');
-   db.rebuildDbSchema();
-}
